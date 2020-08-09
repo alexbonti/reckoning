6cc18cf2af9f130c81f2e955eab6bf522a2742d0
alessio bonti <alexb@83Y7SQ2.du.deakin.edu.au>
2020-08-09 11:43:23 +1000

    application bootstrapped

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-var express = require("express"),
+const MongoClient = require('mongodb').MongoClient;
+express = require("express"),
     app = express();
 
 var port = process.env.PORT || 8080;
@@ -14,3 +15,26 @@ app.listen(port);
 console.log("Listening on port ", port);
 
 require("cf-deployment-tracker-client").track();
+
+
+
+//database connection 
+const uri = "mongodb+srv://sit725:sit725@sit725.xjiqt.mongodb.net/reckoning?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true,useUnifiedTopology: true });
+
+
+
+//Journal Collection handler
+
+let journalCollection;
+const openConnection = (message) => {
+  client.connect((err,db) => {
+    journalCollection = client.db("reckoning").collection("journals");
+    if(!err){
+      console.log('Database Connected')
+    }
+  });
+}
+
+
+openConnection()
